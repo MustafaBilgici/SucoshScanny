a67807a41bf06985e995daa41aa15d68dab40a60
MustafaBilgici <48925906+MustafaBilgici@users.noreply.github.com>
2023-05-05 12:44:02 +0300

    php and java ssti added
    php and ssti functions added.

---
+++ java/javassti.py
@@ -0,0 +1,38 @@
+import os
+import re
+import json
+from termcolor import colored
+
+def find_files_to_check(path):
+    java_files = []
+    for dirpath, dirnames, filenames in os.walk(path):
+        for filename in filenames:
+            if filename.endswith(".java"):
+                java_files.append(os.path.join(dirpath, filename))
+    return java_files
+
+def check_for_ssti(file_path):
+    result = {"file_path": file_path, "vulnerabilities": []}
+    with open(file_path, "r") as file:
+        file_contents = file.read()
+        if re.search(r"\$\{.*\}", file_contents) or re.search(r"\$\!.*\!", file_contents):
+            lines = file_contents.split('\n')
+            for i, line in enumerate(lines):
+                if re.search(r"\$\{.*\}", line) or re.search(r"\$\!.*\!", line):
+                    vulnerability = {"line_number": i+1, "line_content": line}
+                    result["vulnerabilities"].append(vulnerability)
+        else:
+            vulnerability = {"line_number": None, "line_content": None}
+            result["vulnerabilities"].append(vulnerability)
+
+    return result
+
+def main_ssti(path):
+    results = []
+    files_to_check = find_files_to_check(path)
+    for file in files_to_check:
+        result = check_for_ssti(file)
+        results.append(result)
+    print(json.dumps(results, indent=4))
+
+main_ssti(".") # "." dizini ve alt dizinlerinde arama yapar.
